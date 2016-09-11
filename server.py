7028fa376eba254fda881c108b6ed1e2be8bc400
Brandon T. Fields <brandon@cdated.com>
2016-09-11 19:43:07 -0400

    Fix create goal and redirect bugs

--- server.py
+++ server.py
@@ -12,6 +12,9 @@ app = Flask(__name__)
 
 USER="cdated"
 
+GOAL_KEYS = ['name', 'units', 'current', 'total',
+             'group','priority', 'notes']
+
 def groups():
     return db.goals.distinct("group")
 
@@ -36,8 +39,7 @@ def new_goal():
                 }
 
     if request.method == 'POST':
-        for field in ['name', 'units', 'current', 'total', 'notes',
-                      'group','priority']:
+        for field in GOAL_KEYS:
             if field in request.form:
                 new_goal[field] = request.form[field]
 
@@ -46,7 +48,7 @@ def new_goal():
                                  ('endDate', 'T23:59:59')]:
             if dateType in request.form:
                 if request.form[dateType]:
-                    new_goal[field] = request.form[dateType] + offset
+                    new_goal[dateType] = request.form[dateType] + offset
 
         col.insert(new_goal)
 
@@ -68,15 +70,17 @@ def handle_current(goal, value, col, match):
     today = datetime.now().date()
     col.update(match, {'$set': {"updated": str(today)}})
 
+# Occurs when a user clicks the Done Today region and confirms
 @app.route('/goals/<string:goal_id>/clear_today', methods=['POST'])
 def clear_today(goal_id):
     col = db.goals
     update = {'$set': {'done_today': 0}}
     col.update({"uid":USER, "_id": ObjectId(goal_id)}, update)
 
-    referrer = request.form['route']
-    if "group" in referrer:
-        return redirect(referrer)
+    if 'route' in request.form:
+        referrer = request.form['route']
+        if "group" in referrer:
+            return redirect(referrer)
 
     return redirect('/#' + goal_id)
 
@@ -92,7 +96,6 @@ def copy_goal(goal_id):
 
 @app.route('/goals/<string:goal_id>/archive', methods=['POST'])
 def archive_goal(goal_id):
-    print("ACHIVING")
     col = db.goals
     match = {"uid":USER, "_id": ObjectId(goal_id)}
     update = {'$set': {"archived": True}}
@@ -113,8 +116,7 @@ def update_progress(goal_id):
     if request.method == 'POST':
         match = {"uid":USER, "_id":ObjectId(goal_id)}
 
-        for field in ['name', 'units', 'current', 'total',
-                      'notes', 'group', 'priority']:
+        for field in GOAL_KEYS:
             if field in request.form:
                 value = request.form[field]
                 if field == 'current':
@@ -131,27 +133,48 @@ def update_progress(goal_id):
                     update = {'$set': {dateType: value}}
                     col.update(match, update)
 
-        referrer = request.form['route']
-        if "group" in referrer:
-            return redirect(referrer)
+        if 'route' in request.form:
+            referrer = request.form['route']
+            if "group" in referrer:
+                return redirect(referrer)
 
         return redirect('/#' + goal_id)
+
 
     if request.method == 'GET':
         title = "Ananke - " + goal["name"]
         return render_template('goal.html', title=title, goal=goal, groups=groups(),
                                operation='Update')
 
+# Reset done today to zero if the day changed
+def refresh_done_today():
+    col = db.goals
+
+    # Clear out done_todays from yesterdays
+    today = datetime.now().date()
+    goals = col.find({"uid":USER})
+    for goal in goals:
+        updated = goal['updated']
+        last_updated = datetime.strptime(updated, '%Y-%m-%d').date()
+        if last_updated != today:
+            update = {'$set': {'done_today': 0}}
+            col.update({"uid":USER, "_id": goal["_id"]}, update)
+
+
 @app.route('/goals/group/<string:group>', methods=['GET'])
 def group(group):
     col = db.goals
+    refresh_done_today()
+
     goals = col.find({"uid":USER, "group": group, "archived": {"$ne": True}}).sort([("priority", -1), ("endDate", 1)])
 
-    return render_template('index.html', title="Ananke - Archived Goals", goals=goals, groups=groups(), archive='True')
+    title = "Ananke - %s Group" % group
+    return render_template('index.html', title=title, goals=goals, groups=groups(), archive='True')
 
 @app.route('/goals/archived', methods=['GET'])
 def archived():
     col = db.goals
+    refresh_done_today()
     goals = col.find({"uid":USER, "archived": {"$eq": True}}).sort([("priority", -1), ("endDate", 1)])
 
     return render_template('index.html', title="Ananke - Archived Goals", goals=goals, groups=groups(), archive='True')
@@ -159,16 +182,7 @@ def archived():
 @app.route('/')
 def index():
     col = db.goals
-
-    # Clear out done_todays from yesterdays
-    today = datetime.now().date()
-    goals= col.find({"uid":USER})
-    for goal in goals:
-        updated = goal['updated']
-        last_updated = datetime.strptime(updated, '%Y-%m-%d').date()
-        if last_updated != today:
-            update = {'$set': {'done_today': 0}}
-            col.update({"uid":USER, "_id": goal["_id"]}, update)
+    refresh_done_today()
 
     goals = col.find({"uid":USER, "archived": {"$ne": True}}).sort([("priority", -1), ("endDate", 1)])
 
